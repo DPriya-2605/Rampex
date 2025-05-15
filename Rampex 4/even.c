fe3007021356b908b4b7a1a26d6a6c1e1cf00bc6
DPriya-2605 <priyadevaraj2605@gmail.com>
2025-05-15 16:09:50 +0530

    Add files via upload

---
+++ Rampex 4/even.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+  for(int a=0;a<20;a+=2)
+  {
+      printf("%d\n",a);
+  }
+    return 0;
+}
---
+++ Rampex 4/multiplication.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+
+int main(){
+    int num=5;
+    for(int a=1;a<=20;a++)
+    {
+        printf("%d*%d=%d\n",a,num,a*num);
+    }
+     return 0;
+    }
