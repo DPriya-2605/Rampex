ce93767c0b1003d60bf083c4e5477cb3a94533fd
DPriya-2605 <priyadevaraj2605@gmail.com>
2025-05-20 15:55:40 +0530

    Add files via upload

---
+++ Rampex 8/factorial function.c
@@ -0,0 +1,14 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+int factorial(int n)
+{
+    int fact=1;
+    for(int i=1;i<=n;i++){
+        fact=fact*i;
+    }
+
+    return fact;
+}
+int main(){
+    printf("%d",factorial(3));
+}
